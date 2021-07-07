ed277d39eec319eae105eca889047aca8146713e
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-07-07 18:17:46 -0500

    Fix detail on map inside JSX

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -3,18 +3,20 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
 
-const BooksList = ({ books }) => {
-  console.log(books);
-  console.log(books[0].id);
-  // books.map((val) => console.log(val.id));
-  return (
-    <ul className="books-list">
-      {
-        books.map((v) => <Book key={v.id} id={v.id} title={v.title} category={v.category} />)
-      }
-    </ul>
-  );
-};
+const BooksList = ({ books }) => (
+  <ul className="books-list">
+    {
+      books.map((v) => (
+        <Book
+          key={v.id}
+          id={v.id}
+          title={v.title}
+          category={v.category}
+        />
+      ))
+    }
+  </ul>
+);
 
 BooksList.propTypes = {
   books: PropTypes.objectOf(PropTypes.array).isRequired,
